1a47a885f1f1a8d2466e83195d0271ac4d72d5f9
Manalan Kumarasamy Muruganandam <kmmanalan01@gmail.com>
2025-06-25 19:41:41 +1000

    feature:working tray-icon

--- src/main.py
+++ src/main.py
@@ -1,33 +1,15 @@
 import pystray
 from PIL import Image
 
-image = Image.open("src/gfg_image.png")
-
-
 def after_click(icon, query):
-    if str(query) == "GeeksforGeeks Website":
-        print("The Best Place to learn anything Tech \
-      Related -> https://www.geeksforgeeks.org/")
-        # icon.stop()
-    elif str(query) == "GeeksforGeeks Youtube":
-        print("Youtube Channel of GeeksforGeeks \
-      is -> https://www.youtube.com/@GeeksforGeeksVideos")
-        # icon.stop()
-    elif str(query) == "GeeksforGeeks LinkedIn":
-        print("LinkedIn of GeeksforGeeks \
-      is -> https://www.linkedin.com/company/geeksforgeeks/")
-    elif str(query) == "Exit":
+    if str(query) == "Exit":
         icon.stop()
 
+image_path = 'src/sunset-green.png'
 
-icon = pystray.Icon("GFG", image, "GeeksforGeeks", 
-                    menu=pystray.Menu(
-    pystray.MenuItem("GeeksforGeeks Website", 
-                     after_click),
-    pystray.MenuItem("GeeksforGeeks Youtube", 
-                     after_click),
-    pystray.MenuItem("GeeksforGeeks LinkedIn", 
-                     after_click),
-    pystray.MenuItem("Exit", after_click)))
+image = Image.open(image_path)
 
+icon = pystray.Icon("Sunset", image, "Sun Tray", menu=pystray.Menu(pystray.MenuItem("Exit", after_click)))
+
+print('Starting!')
 icon.run()
